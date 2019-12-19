18511f5bde3a6b25f301e8d94d42ae32c82b4782
Qiyd81 <m18621058126@163.com>
2019-12-18 23:56:06 -0800

    Update tl_classifier.py that the car can stop at red and go at green, but not precisely, and sometimes ssize error with resize function

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -23,12 +23,13 @@ from styx_msgs.msg import TrafficLight
 import tensorflow as tf
 import numpy as np
 # import matplotlib.pyplot as plt
-# from PIL import Image
-# from PIL import ImageDraw
-# from PIL import ImageColor
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageColor
 # import time
 # from scipy.stats import norm
 import cv2
+from keras.models import load_model
 
 #%matplotlib inline
 #plt.style.use('ggplot')
@@ -47,6 +48,7 @@ class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
         self.current_light = TrafficLight.UNKNOWN
+        self.light_classes = ['Red', 'Green', 'Yellow']
 
         self.detection_graph = tf.Graph()
         with self.detection_graph.as_default():
@@ -77,8 +79,33 @@ class TLClassifier(object):
         # The classification of the object (integer id).
         self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
 
+        self.light_model = load_model('/home/student/CarND-Capstone/ros/src/tl_detector/light_classification/tl_model_5.h5')
+        self.graph = tf.get_default_graph()
+
 
 #		pass
+    def filter_boxes(self, min_score, boxes, scores, classes):
+        """Return boxes with a confidence >= `min_score`"""
+        n = len(classes)
+        idxs = []
+        for i in range(n):
+            if scores[i] >= min_score:
+                idxs.append(i)
+
+        filtered_boxes = boxes[idxs, ...]
+        filtered_scores = scores[idxs, ...]
+        filtered_classes = classes[idxs, ...]
+        return filtered_boxes, filtered_scores, filtered_classes
+
+    def draw_boxes(self, image, boxes, classes, thickness=4):
+        """Draw bounding boxes on the image"""
+        draw = ImageDraw.Draw(image)
+        for i in range(len(boxes)):
+            bot, left, top, right = boxes[i, ...]
+            # class_id = int(classes[i])
+            # color = COLOR_LIST[class_id]
+            draw.line([(left, top), (left, bot), (right, bot), (right, top), (left, top)], width=thickness)
+            
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
 
@@ -95,7 +122,9 @@ class TLClassifier(object):
         # Load a sample image.
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
-
+        width, height, _ = image.shape
+        # print('image width=', width)
+        # print('image height=', height)
         # with tf.Session(graph=self.detection_graph) as sess:
         with self.detection_graph.as_default():
         # Actual detection.
@@ -106,24 +135,61 @@ class TLClassifier(object):
             scores = np.squeeze(scores)
             classes = np.squeeze(classes).astype(np.int32)
 
-            confidence_cutoff = 0.8
+            confidence_cutoff = 0.15
             light_count = 0
             other_count = 0
             # # Filter boxes with a confidence score less than `confidence_cutoff`
-            # boxes, scores, classes = filter_boxes(confidence_cutoff, boxes, scores, classes)
-
+            boxes, scores, classes = self.filter_boxes(confidence_cutoff, boxes, scores, classes)
+            # self.draw_boxes(image, boxes, classes, thickness=4)
+        red_lights = []
         for i in range(boxes.shape[0]):
-            if scores is None or scores[i] > confidence_cutoff:
-                other_count += 1
-                # class_name = self.category_index[classes[i]]['name']
-                # if class_name == 'Red':
-                if classes[i] == 10:
-                    light_count += 1
-        # if light_count < len(boxes.shape[0])/2:
-        if light_count < other_count/2:
+            if classes[i] == 10:
+                y1 = np.int32(boxes[i][0]*height)
+                y2 = np.int32(boxes[i][2]*height)
+                x1 = np.int32(boxes[i][1]*width)
+                x2 = np.int32(boxes[i][3]*width)
+                # print('x1 =', x1)
+                # print('y1 =', y1)
+                h = y2 - y1
+                w = x2 - x1
+                ratio = h/(w+0.01)
+                if (h < 20) or (w < 20) or (ratio < 0.1) or (ratio > 10):
+                    img_light = np.zeros((1, 32,32, 3), dtype=np.int32)
+                else:
+                    img_light = cv2.resize(image[y1:y2, x1:x2], (32,32))
+                    print('img_light shape=', img_light.shape)
+                    img_light = np.expand_dims(np.asarray(img_light, dtype=np.uint8), 0)
+                    with self.graph.as_default():
+                        light_predict = self.light_model.predict(img_light)
+                        light_color = self.light_classes[np.argmax(light_predict)]
+                        print('light color = ', light_color)
+                        print('classes =', classes[i])
+                    red_lights.append(light_color)
+                        # self.current_light = light_color
+        print('red_lights =', red_lights)
+        # idx = next((i for i, v in enumerate(red_lights) if v == 'Red'), None)
+        if 'Red' in red_lights:
+            self.current_light = TrafficLight.RED
+        elif 'Yellow' in red_lights:
+            self.current_light = TrafficLight.YELLOW
+        elif 'Green' in red_lights:
             self.current_light = TrafficLight.GREEN
         else:
-            self.current_light = TrafficLight.RED
+            self.current_light = TrafficLight.UNKNOWN 
+
+
+        # for i in range(boxes.shape[0]):
+        #     if scores is None or scores[i] > confidence_cutoff:
+        #         other_count += 1
+        #         # class_name = self.category_index[classes[i]]['name']
+        #         # if class_name == 'Red':
+        #         if classes[i] == 10:
+        #             light_count += 1
+        # # if light_count < len(boxes.shape[0])/2:
+        # if light_count < other_count/2:
+        #     self.current_light = TrafficLight.GREEN
+        # else:
+        #     self.current_light = TrafficLight.RED
 
             # # The current box coordinates are normalized to a range between 0 and 1.
             # # This converts the coordinates actual location on the image.
@@ -137,43 +203,24 @@ class TLClassifier(object):
             # plt.imshow(image)
 
         return self.current_light 
-    # def filter_boxes(min_score, boxes, scores, classes):
-    #     """Return boxes with a confidence >= `min_score`"""
-    #     n = len(classes)
-    #     idxs = []
-    #     for i in range(n):
-    #         if scores[i] >= min_score:
-    #             idxs.append(i)
 
-    #     filtered_boxes = boxes[idxs, ...]
-    #     filtered_scores = scores[idxs, ...]
-    #     filtered_classes = classes[idxs, ...]
-    #     return filtered_boxes, filtered_scores, filtered_classes
 
-	# def to_image_coords(boxes, height, width):
-	# 	"""
-	# 	The original box coordinate output is normalized, i.e [0, 1].
+	def to_image_coords(boxes, height, width):
+		"""
+		The original box coordinate output is normalized, i.e [0, 1].
 		
-	# 	This converts it back to the original coordinate based on the image
-	# 	size.
-	# 	"""
-	# 	box_coords = np.zeros_like(boxes)
-	# 	box_coords[:, 0] = boxes[:, 0] * height
-	# 	box_coords[:, 1] = boxes[:, 1] * width
-	# 	box_coords[:, 2] = boxes[:, 2] * height
-	# 	box_coords[:, 3] = boxes[:, 3] * width
+		This converts it back to the original coordinate based on the image
+		size.
+		"""
+		box_coords = np.zeros_like(boxes)
+		box_coords[:, 0] = boxes[:, 0] * height
+		box_coords[:, 1] = boxes[:, 1] * width
+		box_coords[:, 2] = boxes[:, 2] * height
+		box_coords[:, 3] = boxes[:, 3] * width
 		
-	# 	return box_coords
+		return box_coords
 
-	# def draw_boxes(image, boxes, classes, thickness=4):
-	# 	"""Draw bounding boxes on the image"""
-	# 	draw = ImageDraw.Draw(image)
-	# 	for i in range(len(boxes)):
-	# 		bot, left, top, right = boxes[i, ...]
-	# 		class_id = int(classes[i])
-	# 		color = COLOR_LIST[class_id]
-	# 		draw.line([(left, top), (left, bot), (right, bot), (right, top), (left, top)], width=thickness, fill=color)
-			
+
 	# def load_graph(graph_file):
 	# 	"""Loads a frozen inference graph"""
 	# 	graph = tf.Graph()
